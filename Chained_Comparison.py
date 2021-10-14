4af05d379ea6a0867245019f258f7cd0613b0be2
MYasa <nearhxh@gmail.com>
2021-10-14 15:27:44 +0700

    Chained Comparison with python ways

---
+++ Chained_Comparison.py
@@ -0,0 +1,6 @@
+print("General Programming")
+x = 100
+print(f"is the value of x between 90 and 150 = {(x >= 90) and (x <= 150)}")
+
+# python ways
+print(f"comparasion with python ways = {90 <= x <= 150}")
