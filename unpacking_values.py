a899e309f884bd98971da222b4d6a61c2940be67
MYasa <nearhxh@gmail.com>
2021-10-14 16:45:27 +0700

    Unpacking a Set of Values

---
+++ unpacking_values.py
@@ -0,0 +1,17 @@
+print("Unpacking a Set of Values\n")
+print('General Programming')
+
+my_list = [100,200,300]
+a = my_list[0]
+b = my_list[1]
+c = my_list[2]
+print(f"my_list = {my_list}")
+print(f"a={a}, b={b}, c={c}")
+
+
+print('\nPython Ways')
+
+data = [10,20,30]
+x,y,z = data
+print(f"list = {data}")
+print(f"x={x}, y={y}, z={z}")
