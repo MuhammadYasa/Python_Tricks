f4ee83e02f6f1ed5a76f2926494a8e83249902f3
MYasa <nearhxh@gmail.com>
2021-10-14 15:41:04 +0700

    Packing a set of values in a list

---
+++ Packing_values_to_a_list.py
@@ -0,0 +1,20 @@
+print("General Programming")
+x = 111
+y = 222
+z = 333
+L = [None]*3
+L[0] = x
+L[1] = y
+L[2] = z
+print(f"x={x}, y={y}, z={z}")
+print(f"L={L}")
+
+print("\nPython Ways")
+a, b, c = 101, 202, 303
+d = a, b, c
+print(f'a={a}, b={b}, c={c}')
+print(f'd={d}') # d is tupple type
+
+# for get list use this ways
+g = [a, b, c]
+print(f"g={g}")
