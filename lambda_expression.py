94aecf8c993229db53cc5a9f7e2500851c2fa104
MYasa <nearhxh@gmail.com>
2021-10-18 01:38:13 +0700

    lambda expressoin

---
+++ lambda_expression.py
@@ -0,0 +1,11 @@
+mylist = [10, 20, 30]
+newlist = list(map(lambda x: x*2, mylist))
+
+"""
+lambda has 2 parameters
+input : output
+x : x*2
+"""
+
+print(f'mylist : {mylist}')
+print(f'new_list : {newlist}')
