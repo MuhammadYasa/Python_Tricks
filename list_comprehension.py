cbf2ebbdfeaf8c5f18f01adf87601ea93d3d0ffc
MYasa <nearhxh@gmail.com>
2021-10-18 14:09:33 +0700

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,14 @@
+mylist1 = []
+mylist2 = []
+for a in range(1, 21):
+    mylist1.append(a)
+
+for b in range(20):
+    mylist2.append(b+1)
+
+print(f'mylist1 : {mylist1}')
+print(f'mylist2 : {mylist2}')
+
+print('\nPythonic List')
+p_list = [x for x in range(1, 26)]
+print(f'p_list : {p_list}')
